9cded5b8fd9d9fc88c1110d3d7eb4961b0903f13
24bcsl16-Abhijit <cse.24bcsl16@silicon.ac.in>
2025-06-07 11:04:17 +0530

    Assignment Q7 & 8 completed

---
+++ src/DAY3/Q7.kt
@@ -0,0 +1,5 @@
+package DAY3
+
+fun main() {
+    for (price in 1..10) if (price % 2 == 0) println("Coffee for $$price – that's a steal!")
+}
---
+++ src/DAY3/Q8.kt
@@ -0,0 +1,7 @@
+package DAY3
+
+fun main() {
+    for (cups in 5 downTo 1) {
+        println("Only $cups cups left!")
+    }
+}
